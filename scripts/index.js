ba6bc6193ee251f432d724a9eeaed4c54a047b51
Dmitrii Nesterenko <dmitrii.terra@gmail.com>
2024-06-06 19:22:23 +0300

    just noted some ideas

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,21 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+const pageContent = document.querySelector('.places__list');
+// const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+function cardControl (card, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  
+  cardElement.querySelector('.card__image').src = initialCards.link;
+  cardElement.querySelector('.card__title').textContent = initialCards.name;
+  pageContent.append(cardElement);
+  return cardElement;
+}
+
+
+// pageContent.append(cardElement);
+
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
